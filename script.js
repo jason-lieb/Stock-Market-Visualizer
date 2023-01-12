8f6b2fcd39e07394cb0ce3812e944805596809a4
Jason Lieb <Jason.lieb@outlook.com>
2023-01-12 10:39:56 -0500

    initialize files

---
+++ script.js
@@ -0,0 +1,29 @@
+// Global Variables
+
+
+// Query Selectors
+
+
+// Access Data from Polygon API
+
+
+// Access Data from FRED API
+
+
+// Parse Polygon Response
+
+
+// Parse FRED Response
+
+
+// Visualize Data with D3
+
+
+// Update Chart DOM
+
+
+// Clear Chart Container
+
+
+// Change Time Range
+
