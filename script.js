0175680c04ccf965caf1e11369816364d958690b
Jason Lieb <Jason.lieb@outlook.com>
2023-01-12 13:40:41 -0500

    parse alpha vantage response and output data, also create data.json file for testing

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ let alpha_vantage_APIKEY = '0BGSBFE3M96OL784';
 
 
 // Call Functions
-// getPolygon('SPX');
-// let data = getAlphaVantage('IBM');
+async function callFunction() {
+  // getPolygon('SPX');
+  let rawDataAV = await getAlphaVantage('IBM');
+  parseAlphaVantage(rawDataAV);
+}
+
+callFunction()
 
 // Access Data from Polygon API
 async function getPolygon(ticker) {
@@ -21,17 +26,27 @@ async function getAlphaVantage(ticker, outputSize) {
   // If outputSize is 'compact', will only fetch last 100 days; 'full' responds with 20+ years of data
   outputSize = outputSize || 'full';
   let response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=${outputSize}&apikey=${alpha_vantage_APIKEY}`);
-  let data = await response.json();
-  console.log(data);
-  console.log(data['Time Series (Daily)']);
-  return data;
+  let rawData = await response.json();
+  return rawData;
 }
 
 // Access Data from FRED API
 
 
 // Parse Alpha Vantage Response
-
+function parseAlphaVantage(rawData) {
+  let data = rawData['Time Series (Daily)'];
+  let parsedData = [];
+  console.log(rawData)
+  console.log(data);
+  let keys = Object.keys(data);
+  for (let i = 0; i < keys.length; i++) {
+    parsedData.push({ date: keys[i], closeValue: data[keys[i]]['4. close'] });
+  }
+  // Output data in form of array with objects with keys date and closeValue
+  console.log(parsedData);
+  return parsedData;
+}
 
 // Parse FRED Response
 
