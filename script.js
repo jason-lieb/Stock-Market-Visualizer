167fab666040ad28f1dbb454f070a0ca5801af42
Jason Lieb <Jason.lieb@outlook.com>
2023-01-12 11:50:25 -0500

    add functions to get data for tickers and add alpha vantage call for historical data

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 // Global Variables
-polygon_APIKEY = 'TTNbgrcWIJyP1tavyIdjxgTywo6ixljm';
+let polygon_APIKEY = 'TTNbgrcWIJyP1tavyIdjxgTywo6ixljm';
+let alpha_vantage_APIKEY = '0BGSBFE3M96OL784';
 
 // Query Selectors
 
 
-// Access Data from Polygon API
-function getPolygon() {
+// Call Functions
+// getPolygon('SPX');
+// getAlphaVantage('IBM');
 
+// Access Data from Polygon API
+async function getPolygon(ticker) {
+  let response = await fetch(`https://api.polygon.io/v3/reference/tickers/${ticker}?date=2023-01-12&apiKey=${polygon_APIKEY}`);
+  // let response = await fetch('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&include_otc=false&apiKey=TTNbgrcWIJyP1tavyIdjxgTywo6ixljm')
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
+
+// Access Data from Alpha Vantage API
+async function getAlphaVantage(ticker, outputSize) {
+  // If outputSize is 'compact', will only fetch last 100 days; 'full' responds with 20+ years of data
+  outputSize = outputSize || 'full';
+  let response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&outputsize=${outputSize}&apikey=${alpha_vantage_APIKEY}`);
+  let data = await response.json();
+  console.log(data);
+  return data;
 }
 
 // Access Data from FRED API
