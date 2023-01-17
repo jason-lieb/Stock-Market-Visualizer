f14652b50d7f6f62d03477db424a3eb93247063b
Lantao home PC <lantaook@gmail.com>
2023-01-17 01:36:33 -0500

    delete testdata and FRED API

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //API Keys
 let polygon_APIKEY = "TTNbgrcWIJyP1tavyIdjxgTywo6ixljm";
 let alpha_vantage_APIKEY = "0BGSBFE3M96OL784";
-// let fred_APIKEY = "ce4ba2fd678f9dfc7903324adee68449"; //depreciated
 let bea_APIKEY = "D34BBF56-E892-4E7A-9427-83869BD3A09D";
 let finnhub_APIKEY = "cf2ap1aad3idqn4q4nlgcf2ap1aad3idqn4q4nm0";
 
